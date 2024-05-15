ac399d457e91f51414b3bffe4d41d7f459ced8cd
michaelkorvac <58634427+michaelkorvac@users.noreply.github.com>
2024-05-15 16:49:09 -0500

    Update chg1.js

--- change requests/chg1.js
+++ change requests/chg1.js
@@ -1,2 +1,8 @@
+/*
+requested_by:d60a489b47060210e3fba3b3416d43a8
+chg_model:devops
+business_service:c69b36ac73d423002728660c4cf6a71d
+description: change the k8 again please
 */
+const incGr = new GlideRecord('change_request');
   
