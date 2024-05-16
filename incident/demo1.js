2610bc4748eec0ab8677a5f04cef093f90443898
michaelkorvac <58634427+michaelkorvac@users.noreply.github.com>
2024-05-16 08:26:27 -0500

    Update demo1.js

--- incident/demo1.js
+++ incident/demo1.js
@@ -4,6 +4,6 @@ category:software
 subcategory:email
 business_service:c69b36ac73d423002728660c4cf6a71d
 contact_type:email
-description: welcome
+description: close this please, we did it.
 */
 const incGr = new GlideRecord('incident');
